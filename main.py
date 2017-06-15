2e945a730b85ac2656174f89f333a476bf2032b1
vei tsi <xtfkpi+veitsi@gmail.com>
2017-06-15 12:37:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import string
+from pprint import pprint
+
+
+def optimize_data(template, data):
+    pieces = string.Formatter().parse(template)
+    raise NotImplementedError("TODO")
+
+
+def main():
+    template = 'Python version: {languages[python][latest_version]}'
+    data = {
+        'languages': {
+            'python': {
+                'latest_version': '3.6',
+                'site': 'http://python.org',
+            },
+            'rust': {
+                'latest_version': '1.17',
+                'site': 'https://rust-lang.org',
+            },
+        },
+    }
+    print("Original data:")
+    pprint(data)
+
+    new_data = optimize_data(template, data)
+    print("Optimized data:")
+    pprint(new_data)
+
+
+if __name__ == '__main__':
+main()
